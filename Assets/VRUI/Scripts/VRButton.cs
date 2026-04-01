4547a720c09aec752c083c6938fee13c1ebc36a6
08 <user@FIH-COM23a108.fsu.edu>
2026-04-01 15:48:50 -0400

    Add button movement and sfx

--- Assets/VRUI/Scripts/VRButton.cs
+++ Assets/VRUI/Scripts/VRButton.cs
@@ -1,29 +1,86 @@
 using UnityEngine;
-using UnityEngine.XR.Interaction.Toolkit.Interactables;
-using UnityEngine.XR.Interaction.Toolkit.Interactors;
+using UnityEngine.Events;
 
 public class VRButton : MonoBehaviour
 {
-    [SerializeField] private XRSimpleInteractable interactable;
+    [SerializeField] private GameObject buttonVisual;
 
-    private IXRSelectInteractor currentInteractor = null;
+    [SerializeField] private Transform pressTransform;
+    [SerializeField] private float pressDuration = 0.2f;
 
+    [SerializeField] private AudioClip pressSound = null;
+    [SerializeField] private AudioClip releaseSound = null;
+
+    [SerializeField] private UnityEvent pressEvent;
+
+    
+    enum State
+    {
+        Idle,
+        Pressing,
+        Releasing
+    }
+
+    private AudioSource audioSource;
+
+    private State state = State.Idle;
+    private Vector3 initialPosition, pressPosition;
+    private float pressTimer = 0f;
+    
     public void SelectEnter()
     {
-        currentInteractor = interactable.firstInteractorSelecting;
+        pressEvent.Invoke();
+
+        if (pressSound != null)
+            audioSource.PlayOneShot(pressSound);
+        
+        state = State.Pressing;
+        pressTimer = 0f;
     }
 
     public void SelectExit()
     {
-        currentInteractor = null;
-        Debug.Log("Select exit");
+        pressEvent.Invoke();
+
+        if (releaseSound != null)
+            audioSource.PlayOneShot(releaseSound);
+
+        state = State.Releasing;
+        pressTimer = 0f;
+    }
+
+    private void Start()
+    {
+        initialPosition = buttonVisual.transform.position;
+        pressPosition = pressTransform.position;
+
+        audioSource = GetComponent<AudioSource>();
     }
 
     private void Update()
     {
-        if (currentInteractor != null)
+        if (state != State.Idle)
         {
-            Debug.Log(currentInteractor.GetAttachTransform(interactable).position);
+            pressTimer = Mathf.MoveTowards(pressTimer, pressDuration, Time.deltaTime);
+            float t = pressTimer / pressDuration;
+            Vector3 newPosition = Vector3.zero;
+
+            switch (state)
+            {
+                case State.Pressing:
+                    newPosition = Vector3.Lerp(initialPosition, pressPosition, t);
+                    break;
+                case State.Releasing:
+                    newPosition = Vector3.Lerp(pressPosition, initialPosition, t);
+                    break;
+            }
+
+            buttonVisual.transform.position = newPosition;
+
+            if (pressTimer == pressDuration)
+            {
+                state = State.Idle;
+            }
         }
     }
 }
